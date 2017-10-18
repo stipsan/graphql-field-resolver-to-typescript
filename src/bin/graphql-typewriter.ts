74fb2ebcfacbb3176d1fe93554da11e383a14a1a
Stian Didriksen <stipsan@gmail.com>
2017-10-19 00:56:57 +0200

    refactor to satisfy compiler

---
+++ src/bin/graphql-typewriter.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import program = require('commander')
+import { runCli } from '../runCli'
+
+function stringArray(arg: any, message: string): string[] {
+  if (!arg) {
+    throw new Error(`${message}: Value not set`)
+  }
+  if (
+    (arg instanceof Array && arg.length === 0) ||
+    typeof arg[0] === 'string'
+  ) {
+    return arg
+  }
+  throw new Error(message + ': Not a string[]')
+}
+
+program
+  .version(require('../package.json').version)
+  .description(
+    'Convert all .graphqls schema-files in the current directory tree into typescript\n' +
+      'interfaces that can be used to implement a graphql-root for this schema.'
+  )
+  .option(
+    '-x, --exclude <dirs>',
+    'a list of directories to exclude',
+    (current, last) => last.concat([current]),
+    ['node_modules']
+  )
+  .option(
+    '--dont-save-same-file',
+    'do not save a file if the contents has not changed. ' +
+      'This read each target file prior to loading'
+  )
+  .option(
+    '-i, --input <files>',
+    'a list of files to parse instead of globbing *.graphqls',
+    (current, last) => last.concat([current]),
+    []
+  )
+  .parse(process.argv)
+
+runCli({
+  input: stringArray(program['input'], "Verification of 'input'-parameter"),
+  exclude: stringArray(
+    program['exclude'],
+    "Verification of 'exclude'-parameter"
+  ),
+  dontSaveSameFile: Boolean(program['dontSaveSameFile']),
+}).then(() => console.log('done'))
---
+++ src/test/schemas/simpleSchema.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+
+export namespace schema {
+  export type GraphqlField<Args, Result, Ctx> =
+    | Result
+    | Promise<Result>
+    | ((args: Args, context: Ctx) => Result | Promise<Result>)
+
+  export interface Query<Ctx> {
+    /**
+         * A field description
+         */
+    field1?: GraphqlField<{}, TypeA<Ctx> | undefined, Ctx>
+    /**
+         * Another field description
+         */
+    field2?: GraphqlField<{}, TypeB<Ctx> | undefined, Ctx>
+  }
+
+  /**
+     * A simple type
+     * Multiline description
+     */
+  export interface TypeA<Ctx> {
+    name?: GraphqlField<{}, string | undefined, Ctx>
+    size?: GraphqlField<{}, number | undefined, Ctx>
+  }
+
+  /**
+     * Another more complex type
+     */
+  export interface TypeB<Ctx> {
+    nested?: GraphqlField<{}, (TypeA<Ctx> | undefined)[] | undefined, Ctx>
+  }
+
+  export const defaultResolvers = {}
+}
