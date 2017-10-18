61949e1b4335390133582c3084c2bad9065d5479
Stian Didriksen <stipsan@gmail.com>
2017-10-19 01:55:13 +0200

    add note

--- src/renderTag.ts
+++ src/renderTag.ts
@@ -1,11 +1,14 @@
-const spaces = '                                                                                       '
+// @TODO probably possible to replace this with prettier
+
+const spaces =
+  '                                                                                       '
 
 /**
  * Token that removes whitespace following the substition up to and including the next newline character
  * @type {{action: string}}
  */
 export const OMIT_NEXT_NEWLINE = {
-    action: 'omit next newline'
+  action: 'omit next newline',
 }
 
 /**
@@ -23,20 +26,29 @@ export const OMIT_NEXT_NEWLINE = {
  * @param a6 string substitution
  * @param a7 string substitution
  */
-export function source(array, a1, a2?, a3?, a4?, a5?, a6?, a7? /* dynamic args */) {
-    const args = Array.prototype.slice.call(arguments, 1)
-    let result = array[0]
-    for (let i = 0; i < args.length; i++) {
-        // Determine indent
-        const indent = result.length - result.lastIndexOf('\n') - 1
-        switch (args[i]) {
-            case OMIT_NEXT_NEWLINE:
-                result += array[i + 1].replace(/^ *\n/, '')
-                break
-            default:
-                result += String(args[i]).replace(/\n/g, '\n' + spaces.slice(0, indent))
-                result += array[i + 1]
-        }
+export function source(
+  array,
+  a1,
+  a2?,
+  a3?,
+  a4?,
+  a5?,
+  a6?,
+  a7? /* dynamic args */
+) {
+  const args = Array.prototype.slice.call(arguments, 1)
+  let result = array[0]
+  for (let i = 0; i < args.length; i++) {
+    // Determine indent
+    const indent = result.length - result.lastIndexOf('\n') - 1
+    switch (args[i]) {
+      case OMIT_NEXT_NEWLINE:
+        result += array[i + 1].replace(/^ *\n/, '')
+        break
+      default:
+        result += String(args[i]).replace(/\n/g, '\n' + spaces.slice(0, indent))
+        result += array[i + 1]
     }
-    return result.replace(/(^\n|\n$)/g, '')
+  }
+  return result.replace(/(^\n|\n$)/g, '')
 }
