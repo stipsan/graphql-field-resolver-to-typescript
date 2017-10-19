e51a70250954993d01a08bbfb06b65b2f970c3db
Stian Didriksen <stipsan@gmail.com>
2017-10-19 02:40:23 +0200

    Fixes and refactoring

--- src/bin.ts
+++ src/bin.ts
@@ -2,46 +2,36 @@
 
 // @TODO simplify, a lot
 
-import program = require('commander')
 import { runCli } from './runCli'
+import * as meow from 'meow'
 
-function stringArray(arg: any, message: string): string[] {
-  if (!arg) {
-    throw new Error(`${message}: Value not set`)
+const cli = meow(
+  [
+    'Usage:',
+    '  $ gqlfr2ts [<input> ...]',
+    '',
+    'Options:',
+    '  --output, -o path          Specifies the name and location of the output file. If not specified, stdout is used.',
+    '',
+    'Examples:',
+    '  $ gqlfr2ts schema.graphql',
+    '    Processes the file schema.graphql and prints to stdout',
+    '  $ cat schema.graphql | gqlfr2ts --output schema.d.ts',
+    '    Processes the input from stdin and writes output to schema.d.ts',
+    '  $ gqlfr2ts RootQuery.graphql User.graphql --output schema.d.ts',
+    '    Stitch a schema together and output a complete typescript definition for all the related resolvers',
+    '  $ cat **/*.graphql | gqlfr2ts > schema.d.ts',
+    '    Stitch a schema together from stdin and pipe it to stdout and use the shell to write output to file.',
+    '    This is the fastest way to do it when you have a lot of files that combine to a big schema.',
+  ],
+  {
+    string: ['output'],
+    alias: {
+      o: 'output',
+      h: 'help',
+    },
   }
-  if (
-    (arg instanceof Array && arg.length === 0) ||
-    typeof arg[0] === 'string'
-  ) {
-    return arg
-  }
-  throw new Error(message + ': Not a string[]')
-}
-
-program
-  .version(require('../package.json').version)
-  .description(
-    'Convert all .graphql schema-files in the current directory tree into typescript\n' +
-      'interfaces that can be used to implement a graphql-root for this schema.'
-  )
-  .option(
-    '-x, --exclude <dirs>',
-    'a list of directories to exclude',
-    (current, last) => last.concat([current]),
-    ['node_modules']
-  )
-  .option(
-    '--dont-save-same-file',
-    'do not save a file if the contents has not changed. ' +
-      'This read each target file prior to loading'
-  )
-  .option(
-    '-i, --input <files>',
-    'a list of files to parse instead of globbing *.graphql',
-    (current, last) => last.concat([current]),
-    []
-  )
-  .parse(process.argv)
+)
 
 runCli({
   input: stringArray(program['input'], "Verification of 'input'-parameter"),
